28c00eb7f2b193cb129225655ac8e5271736c817
Mandinz <andrewisatree@Mandinzs-MacBook.local>
2014-05-28 15:42:09 +0100

    verify address in same was email

--- DomainModellingFSharp/program.fs
+++ DomainModellingFSharp/program.fs
@@ -35,33 +35,58 @@ type PersonalName = {
     LastName: string
 }
 
-type Address = {
-    Address1: string
-    Address2: string
-    City: string
-    County: string
-    PostCode: string
-}
+module AddressInfo =
+    open System
+    type Address = {
+        Address1: string
+        Address2: string
+        City: string
+        County: string
+        PostCode: string
+    }
 
-type AddressInfo = {
-    Address: Address
-    //true if validated against address service
-    IsAddressVerified: bool 
-}
+    type UnverifiedData = Address
+        type VerifiedData = Address * DateTime
+        type T = 
+            | UnverifiedState of UnverifiedData
+            | VerifiedState of VerifiedData
+
+    let create address = 
+        UnverifiedState address
+
+    let verified addressInfo dateVerified =
+        match addressInfo with 
+        | UnverifiedState address ->
+            VerifiedState(address,dateVerified)
+        | VerifiedState _ ->
+            addressInfo
+
+    let sendVerifactionEmail addressInfo = 
+        match addressInfo with 
+        | UnverifiedState address ->
+            //send address
+            printfn "Sending Email"
+        | VerifiedState _ ->
+            //do nothing
+            printfn "Not sending an email -- address already verified"        
+
 
 type Contact = {
     PersonalName: PersonalName
     EmailContactInfo: EmailContactInfo.T
-    AddressInfo: AddressInfo
+    AddressInfo: AddressInfo.T
 }
 
 let personalName = { FirstName = "Ian"; MiddleInitial = Some "J"; LastName = "Russell" }
 let emailContactInfo =  EmailContactInfo.create "hello@add.com"
-let address = { Address1 = "road"; Address2 = "street";City = "London"; County="east"; PostCode = "m22 222" }
-let addressInfo = { Address= address; IsAddressVerified = false }
+
+let addressInfo = AddressInfo.create{ Address1 = "road"; Address2 = "street";City = "London"; County="east"; PostCode = "m22 222" }
 let contact = { PersonalName= personalName; EmailContactInfo= emailContactInfo; AddressInfo= addressInfo }
 
 printfn "%s" contact.PersonalName.LastName
 let foo  = EmailContactInfo.verified emailContactInfo System.DateTime.Now
 EmailContactInfo.sendVerifactionEmail emailContactInfo
 EmailContactInfo.sendVerifactionEmail foo
+
+printfn "%s" (contact.AddressInfo |> string) 
+printfn "%s" (AddressInfo.verified contact.AddressInfo System.DateTime.Now |> string)
