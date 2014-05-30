b06fa7298f68f847a23b81c5b5b8640bef9c34a9
AndrewKindleysides <AndrewKindleysides@gmail.com>
2014-05-30 15:48:35 +0100

    no idea what this was

--- ReactiveUI/src/exercises/ex1-readonlyproperties/Ex1.Tests/PersonViewModelTests.cs
+++ ReactiveUI/src/exercises/ex1-readonlyproperties/Ex1.Tests/PersonViewModelTests.cs
@@ -1,34 +1,35 @@
-﻿using NUnit.Framework;
+﻿using NUnit;
+using NUnit.Framework;
 
 namespace Ex1.Tests
 {
-    [TestFixture]
+	[TestFixture ()]
     public class PersonViewModelTests
     {
-        [Test]
+		[Test ()]
         public void FullNameIsMadeUpFromFirstNameAndLastName()
         {
             var sut = new PersonViewModel {FirstName = "Jane", LastName = "Appleseed"};
 
-            Assert.AreEqual("Jane Appleseed", sut.FullName);
+			Assert.AreEqual("Jane Appleseed", sut.FullName);
         }
 
-        [Test]
+		[Test ()]
         public void FullNameUpdatesWhenFirstNameChanges()
         {
             var sut = new PersonViewModel {FirstName = "Jane", LastName = "Appleseed"};
             sut.FirstName = "Joe";
 
-            Assert.AreEqual("Joe Appleseed", sut.FullName);
+			Assert.AreEqual("Joe Appleseed", sut.FullName);
         }
 
-        [Test]
+		[Test ()]
         public void FullNameUpdatesWhenLastNameChanges()
         {
             var sut = new PersonViewModel {FirstName = "Jane", LastName = "Appleseed"};
             sut.LastName = "Smith";
 
-            Assert.AreEqual("Jane Smith", sut.FullName);
+			Assert.AreEqual("Jane Smith", sut.FullName);
         }
     }
 }
--- ReactiveUI/src/exercises/ex1-readonlyproperties/Ex1/PersonViewModel.cs
+++ ReactiveUI/src/exercises/ex1-readonlyproperties/Ex1/PersonViewModel.cs
@@ -6,17 +6,31 @@ namespace Ex1
     {
         public PersonViewModel()
         {
-            //TODO: Add code to update FullName when FirstName or LastName change
+
         }
 
         //TODO: Add a read-only property for FullName
 
         private string _firstName;
+		private string _lastName;
+		private string _fullName;
+
+		public string FullName
+		{
+			get { return _firstName+  " " + _lastName; }
+		}
+
         public string FirstName
         {
             get { return _firstName; }
             set { this.RaiseAndSetIfChanged(ref _firstName, value); }
         }
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set { this.RaiseAndSetIfChanged(ref _lastName, value); }
+		}
 
         //TODO: Add a second read-write property for LastName
     }
